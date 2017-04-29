bf39ed8958b91cf5573213e61824ecc3eb49e1b6
fver1004 <fver1004@gmail.com>
2017-04-29 14:23:20 +0900

    initializing item.py

--- scrapyTest/items.py
+++ scrapyTest/items.py
@@ -6,6 +6,15 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
+
+class BookItem(scrapy.Item):
+        bookType = scrapy.Field()
+        bookName = scrapy.Field()
+        author = scrapy.Field()
+        publisher = scrapy.Field()
+        symbol = scrapy.Field()
+        date = scrapy.Field()
 
 
 class ScrapytestItem(scrapy.Item):
