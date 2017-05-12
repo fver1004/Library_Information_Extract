f7326e5b11bd83c20e0c94885b60b4c1311626ac
mosmos18 <mosmos18@naver.com>
2017-05-12 18:57:35 +0900

    Update autoscrapy.sh
    시간을 변수로 사용시 인자가 많은 문제가 발생.
    while문과 if문 sleep을 제외하고
    단일 명령을 수행하게 한 후 리눅스의 crontab을 활용하여 주기적 실행이 가능하도록 수정

--- autoscrapy.sh
+++ autoscrapy.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-# 현재 시간을 알기 위한 변수 선언
-NOW= $(date +"%H")
-date= $(date +"%m%d")
-# 금일 스크래피 사용 확인을 위한 Count 변수 추가
-count= 0
-while true;
-	do
-		if [$NOW = 1 -a count = 1]
-		then
-			count = 0
-			fi
-			if [$NOW = 1 -a count = 0 ]
-			then
-				# 스크래피 명령 실행
-				scrapy runspider DaeguLibSpider.py
-				# HDFS 상으로 CSV input
-				hadoop fs -put $HOME/Scarpy*.csv 
-				# 스크래피 분석 Map Ruduce 실행
-				hadoop jar DeaguLibCount.java /input /output
-				count= 1
-				# 끝난 시간을 log날짜.txt에 저장
-				date > ~/log$[date].txt
-				fi
-	done
+# 오늘 날짜를 확인하기 위한 변수 선언
+NOW=$(date +"%m%d")
+# 결과 파일을 분류 저장하기 위한 폴더 생성
+mkdir ~/result/$NOW
+# 스크래피 명령 실행
+scrapy runspider $SCRAPY_HOME/DaeguLibSpider.py
+# HDFS 상에 일별 폴더 생성
+hadoop fs -mkdir /in_$NOW
+# HDFS 상으로 CSV input
+hadoop fs -put ~/result/$NOW/* 
+# 스크래피 분석 Map Ruduce 실행
+hadoop jar ~/DaeguLibCounter.jar DeguLinCounter.DaeguLibCount /$NOW /out_$NOW
+
+#---------------------------------#
+# 일단위 크롤링을 위한 crontab 설정 #
+#  0 1 * * * $path/autoscrapy.sh  #
+#     매일 새벽 한시 크롤링 실행    #
+#---------------------------------#
